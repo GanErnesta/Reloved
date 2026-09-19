5db0732562f2dfb70a1089463830171062594f14
ZIZZYMI OFFICIAL <karsakds@gmail.com>
2026-09-19 11:53:16 +0700

    feat(auth): add persistent login session

--- lib/features/onboarding/pages/onboarding_page2.dart
+++ lib/features/onboarding/pages/onboarding_page2.dart
@@ -1,6 +1,5 @@
 import 'package:application_reloved/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
-import '../../../core/constants/app_spacing.dart';
 import '../../../core/constants/app_text_styles.dart';
 
 class OnboardingPage2 extends StatelessWidget {
--- lib/features/profile/screens/profile_setup_screen.dart
+++ lib/features/profile/screens/profile_setup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../../app/navigation/main_screen.dart';
 import '../../../core/config/supabase_config.dart';
 import '../../auth/models/campus.dart';
 import '../../auth/repositories/campus_repository.dart';
